546e1d151be98f5ee11795c762dd8f2d5a00cad1
Floppymnt <96642794+Floppymnt@users.noreply.github.com>
2025-07-18 10:07:51 -0500

    L3: FPS

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def main():
     pass_fail = pygame.init()
     print(f"pygame initialized with {pass_fail[0]} modules loaded successfully and {pass_fail[1]} modules failed to load.")
     
+
+    #creat a clock object for fps cap
+    clock = pygame.time.Clock()
+    dt = 0
+    print("Clock initialized.")
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
     print(f"Initialized screen with:\nwidth: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
@@ -23,7 +28,7 @@ def main():
 
 
             if event.type == pygame.QUIT: #quit the game
-                print("USER EXIT EVENT: closing with exit code 0")
+                print("USER EXIT EVENT: exiting game loop.")
                 # pygame.quit()
                 return
 
@@ -31,6 +36,9 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+       
+        dt =  clock.tick(FPS) / 1000.0 # cap the frame rate to FPS and get delta time
 
 
 
